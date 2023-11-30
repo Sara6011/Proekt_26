d12107fe9d0b9d2ef901ba70987bec04c66506fa
Jankulovska <jankulovskasara60@gmail.com>
2023-11-30 09:38:33 +0100

    Add first java program

---
+++ src/zadacaaa25/Animal.java
@@ -0,0 +1,9 @@
+package zadacaaa25;
+
+public class Animal {
+ void bark() {
+ System.out.println("Woof-Woof");
+ 
+	
+}
+}
---
+++ src/zadacaaa25/MyClass.java
@@ -0,0 +1,7 @@
+package zadacaaa25;
+public class MyClass {
+public static void main(String [] args) {
+		MyClass dog=new MyClass();
+		dog.bark();
+	}
+}
